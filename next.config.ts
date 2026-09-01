8f2e0b76568a80f8cc85cd4328ab4dddf3d438d7
ompa <nmaladina@hotmail.com>
2026-09-02 00:52:58 +1200

    chore: configure Next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repo = "aws-cloud-assessment";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
